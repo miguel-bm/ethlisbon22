a9c1315a877494b0ad9c0f604dc7d857010492b1
miguel-bm <miguel.blanco.marcos@gmail.com>
2022-10-29 20:26:17 +0100

    add curve interpolator

--- api/utils/curve_interpolator.py
+++ api/utils/curve_interpolator.py
@@ -4,8 +4,10 @@ from api.models.accumulated_debt_point import AccumulatedDebtPoint
 def interpoate_debt_curve(
     curve_data: dict, price_descent: float
 ) -> AccumulatedDebtPoint:
-    return AccumulatedDebtPoint(
-        accumulatedLiquidations=0.0,
-        unit="USD",
-        slippage=0.0,
-    )
+    for data_point in curve_data:
+        if data_point["price_change"] >= price_descent:
+            return AccumulatedDebtPoint(
+                accumulatedLiquidations=data_point["total_liquidation"],
+                unit="USD",
+                slippage=data_point["liquidation_slippage"],
+            )
--- etl/apply_model.py
+++ etl/apply_model.py
@@ -1,13 +1,34 @@
 import pandas as pd
 import json
 import numpy as np
+from pathlib import Path
 from collections import OrderedDict
+from typing import Optional
 
-TOTAL_MAKETS_PATH = "additional_data/total_markets_coingecko.csv"
-ETHEREUM_COMPOUND_PATH = "data/users/compound_data.json"
+TOTAL_MAKETS_PATH = Path("data/total_markets_coingecko.csv")
+ETHEREUM_COMPOUND_PATH = Path("data/users/ethereum_compound.json")
 
 RATIO_COMPOUND_INDUSTRY = 0.1
 LIQUIDATION_THESHOLD = 1
+
+
+STABLECOIN_MARKETS = [
+    "0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643",
+    "0x39AA39c021dfbaE8faC545936693aC917d5E7563",
+    "0xf650C3d88D12dB855b8bf7D11Be6C55A4e07dCC9",
+    "0x12392F67bdf24faE0AF363c24aC620a2f67DAd86",
+]
+STABLECOIN_NAMES = ["DAI", "USDC", "USDT", "TUSD"]
+
+
+def get_user_stablecoin_collateral(user_markets_data: dict) -> float:
+    return np.sum(
+        [
+            markets_data["collateral"]
+            for markets_data in user_markets_data["markets"]
+            if markets_data["market"] in STABLECOIN_MARKETS
+        ]
+    )
 
 
 def get_slippage_dollars(sale_amount: float, market_size: float):
@@ -16,11 +37,16 @@ def get_slippage_dollars(sale_amount: float, market_size: float):
 
 def get_total_market_size() -> float:
     df = pd.read_csv(TOTAL_MAKETS_PATH)
+
     df["2% Depth"] = pd.to_numeric(
-        df["2% Depth"].str.replace(",", "").str.replace("$", "")
+        df["2% Depth"]
+        .str.replace(",", "", regex=False)
+        .str.replace("$", "", regex=False)
     )
     df["-2% Depth"] = pd.to_numeric(
-        df["-2% Depth"].str.replace(",", "").str.replace("$", "")
+        df["-2% Depth"]
+        .str.replace(",", "", regex=False)
+        .str.replace("$", "", regex=False)
     )
 
     df["up_depth"] = df["2% Depth"].apply(lambda x: x * 0.98 * 100)
@@ -30,14 +56,12 @@ def get_total_market_size() -> float:
     return df[df["Pair"].str.contains("/US")]["virtual_total_market"].sum()
 
 
-def impact_lower_price(price_change):
+def get_market_eth_ratio(eth_compound) -> float:
 
-    eth_compound = json.load(open("additional_data/ethereum_compound.json"))
-    users_with_markets = [
-        i for i in eth_compound["users"] if len(i["markets"]) > 0
-    ]  # clean empty users
+    # Clean empty users
+    users_with_markets = [u for u in eth_compound["users"] if len(u["markets"]) > 0]
 
-    m_status = json.load(open("additional_data/compmound_markets_status.json"))
+    m_status = json.load(open("data/markets/compmound_markets_status.json"))
     m_dict = dict(zip([i for i in m_status], [m_status[i]["symbol"] for i in m_status]))
 
     # get the ratio of collateral that ETH represents
@@ -57,31 +81,27 @@ def impact_lower_price(price_change):
         )
     df_collaterals = pd.DataFrame({"collateral": collaterals, "value": values})
     df_collaterals_ns = df_collaterals[
-        ~df_collaterals.collateral.isin(["DAI", "USDC", "USDT", "TUSD"])
+        ~df_collaterals["collateral"].isin(STABLECOIN_NAMES)
     ]
-    df_collaterals_ns.value = df_collaterals_ns.value / df_collaterals_ns.value.sum()
+    value = df_collaterals_ns.set_index("collateral")["value"]
+    norm_value = value / value.sum()
+    return norm_value["ETH"]
 
-    ratio_eth = df_collaterals_ns[df_collaterals_ns.collateral == "ETH"].values[0][1]
 
-    # discount stablecoins
-    stable_coins_markets = [
-        "0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643",
-        "0x39AA39c021dfbaE8faC545936693aC917d5E7563",
-        "0xf650C3d88D12dB855b8bf7D11Be6C55A4e07dCC9",
-        "0x12392F67bdf24faE0AF363c24aC620a2f67DAd86",
-    ]
+def main():
+    total_market_size = get_total_market_size()
 
-    def get_stable_coins(x):
-        return np.sum(
-            [
-                i["collateral"]
-                for i in x["markets"]
-                if i["market"] in stable_coins_markets
-            ]
-        )
+    with ETHEREUM_COMPOUND_PATH.open("r") as f:
+        eth_compound = json.load(f)
+
+    ratio_eth = get_market_eth_ratio(eth_compound)
+    users_with_markets = [u for u in eth_compound["users"] if len(u["markets"]) > 0]
 
     users_with_markets_and_stable_collat = [
-        {**user_data, **{"stable_collateral": get_stable_coins(user_data)}}
+        {
+            **user_data,
+            **{"stable_collateral": get_user_stablecoin_collateral(user_data)},
+        }
         for user_data in users_with_markets
     ]
 
@@ -104,8 +124,8 @@ def impact_lower_price(price_change):
         if u["liquidation_value"] > 0
     ]
 
-    def get_liquidation_percentage(x, y):
-        return y / x
+    def get_liquidation_percentage(x: float, y: float) -> Optional[float]:
+        return y / x if x > 0 else None
 
     users_with_markets_and_stable_collat_with_liquidation = [
         {
@@ -118,12 +138,13 @@ def impact_lower_price(price_change):
             },
         }
         for user_data in users_with_markets_and_stable_collat_with_liquidation
+        if (user_data["collateral"] - user_data["stable_collateral"] > 0)
     ]
 
     df_users = pd.DataFrame(users_with_markets_and_stable_collat_with_liquidation)
     df_users["eth_collateral"] = df_users["collateral"] - df_users["stable_collateral"]
     df_users = df_users[
-        (df_users.eth_collateral > 0) & (df_users.netValue > 0)
+        (df_users["eth_collateral"] > 0) & (df_users["netValue"] > 0)
     ].sort_values("liquidation_perc", ascending=False)
 
     df_users["total_liquidation"] = (
@@ -132,33 +153,25 @@ def impact_lower_price(price_change):
     df_users["price_change"] = 1 - df_users["liquidation_perc"]
 
     df_users["liquidation_slippage"] = get_slippage_dollars(
-        df_users["total_liquidation"] / RATIO_COMPOUND_INDUSTRY, total_market
+        df_users["total_liquidation"] / RATIO_COMPOUND_INDUSTRY, total_market_size
     )  # tenemos que añadir la proporcion de esto del mercado
     df_users["price_after_slippage"] = (
         df_users["liquidation_perc"] * df_users["liquidation_slippage"]
     )
+    # round price change to 4 decimals
+    df_users["price_change"] = df_users["price_change"].apply(lambda x: round(x, 4))
+    df_users = df_users.groupby("price_change").agg(
+        total_liquidation=pd.NamedAgg(column="total_liquidation", aggfunc="max"),
+        liquidation_slippage=pd.NamedAgg(column="liquidation_slippage", aggfunc="min"),
+    )
+    df_users.reset_index(inplace=True)
 
-    market_effects = {
-        "price_change": df_users[df_users.price_change > price_change]["price_change"]
-        .head(1)
-        .values[0],
-        "total_liquidation": df_users[df_users.price_change > price_change][
-            "total_liquidation"
-        ]
-        .head(1)
-        .values[0],
-        "liquidation_slippage": df_users[df_users.price_change > price_change][
-            "liquidation_slippage"
-        ]
-        .head(1)
-        .values[0],  # this is the multiplier of the price
-    }
-
-    return market_effects
-
-
-def main():
-    total_market_size = get_total_market_size()
+    df_users[["price_change", "total_liquidation", "liquidation_slippage"]].to_json(
+        "data/cached_curves/eth.json",
+        orient="records",
+        indent=4,
+        double_precision=4,
+    )
 
 
 if __name__ == "__main__":
